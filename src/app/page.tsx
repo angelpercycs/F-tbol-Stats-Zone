640642a1c9133c30eb010d06b8e1c962a32972f0
angelpercycs <percycsa@hotmail.com>
2025-07-14 09:23:52 -0500

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import FootballApp from '@/components/football-app';
+
+export default async function Home() {
+  return (
+    <main className="min-h-screen bg-background text-foreground">
+      <FootballApp />
+    </main>
+  );
+}
