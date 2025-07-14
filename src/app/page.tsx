f7ed593c52382bafb27c1b2689763973d3991efe
angelpercycs <percycsa@hotmail.com>
2025-07-14 09:49:59 -0500

    Delete src/app directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import FootballApp from '@/components/football-app';
-
-export default async function Home() {
-  return (
-    <main className="min-h-screen bg-background text-foreground">
-      <FootballApp />
-    </main>
-  );
-}
